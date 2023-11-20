7d2f79da12dcc5c395748d240aa85dde03244c53
Dhairya Milindkumar Gundechia <gundechia.d@northeastern.edu>
2023-11-19 19:58:19 -0500

    Added Teacher Class

--- src/main/java/com/neu/csye6200/daycare/DaycareApplication.java
+++ src/main/java/com/neu/csye6200/daycare/DaycareApplication.java
@@ -8,8 +8,10 @@ public class DaycareApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DaycareApplication.class, args);
-        Student student = new Student("S01", "John", "abc@gmail.com", 5, "James", "Mary", "123 Main St", "1234567890");
-        student.insertStudentIntoDatabase(student);
+        //Student student = new Student("S01", "John", "abc@gmail.com", 5, "James", "Mary", "123 Main St", "1234567890");
+        //student.insertStudentIntoDatabase(student);
+        Teacher teacher = new Teacher("T01", "John", "def@gmail.com", 25, 10.0);
+        teacher.insertTeacherIntoDatabase(teacher);
     }
 
 }
